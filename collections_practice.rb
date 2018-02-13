8d9f61873314d315d54c8d50aace531bb8c1ee4a
Harim Choi <harim1206@gmail.com>
2018-02-13 21:36:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
 # your code goes here
+
+def begins_with_r(arr)
+  arr.each do |name|
+    if !name.start_with?('r')
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(arr)
+
+  arr.select{
+    |name| name.include?("a")
+  }
+
+end
+
+def first_wa(arr)
+
+  arr.find{
+    |name|
+    name.start_with?("wa")
+  }
+
+end
+
+def remove_non_strings(arr)
+
+  arr.delete_if{
+    |item|
+    !item.instance_of? String
+  }
+
+end
